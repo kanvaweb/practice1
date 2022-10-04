1d35b6a93a1ebba35767bf3d68f78d800e13c057
Andrey Kislitsyn <kanvaweb@gmail.com>
2022-10-04 19:18:26 +0700

    Все четыре задачи для сдачи

---
+++ Program1-1.cs
@@ -0,0 +1,22 @@
+﻿// Домашнее задание к уроку 1 по C#
+
+// ------------- НАЧАЛО
+// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Введите число 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+    Console.WriteLine($"Число {num1} больше числа {num2}");
+else if (num1 < num2)
+    Console.WriteLine($"Число {num2} больше числа {num1}");
+else
+    Console.WriteLine($"Число {num1} равно числу {num2}");
+// ------------- ОКОНЧАНИЕ
+
+
---
+++ Program1-2.cs
@@ -0,0 +1,23 @@
+﻿// Домашнее задание к уроку 1 по C#
+
+// ------------- НАЧАЛО
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите число 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 3: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 >= num2 && num1 >= num3)
+    Console.WriteLine($"Число {num1} максимальное");
+else if (num2 >= num1 && num2 >= num3)
+    Console.WriteLine($"Число {num2} максимальное");
+else if (num3 >= num1 && num3 >= num2)
+    Console.WriteLine($"Число {num3} максимальное");
+// ------------- ОКОНЧАНИЕ
+
